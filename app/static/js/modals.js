9d17b8a2f24b5e5a523f870a945f933d32863316
geer26 <gergo.kurucz@gmail.com>
2021-09-26 14:06:08 +0200

    v0.1

--- app/static/js/modals.js
+++ app/static/js/modals.js
@@ -1,3 +1,20 @@
+close_modal();
+
 function close_modal(){
     $('.modalback').hide();
+}
+
+
+function show_login(){
+    close_modal();
+    $('#loginback').show();
+}
+
+function show_reg(){
+    close_modal();
+    $('#regback').show();
+}
+
+function login(){
+    console.log('LOGIN TRIGGERED');
 }
